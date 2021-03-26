eca3da713cb64b76f2b341926f221bbce8cb151e
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-26 18:58:39 +0000

    added material ui dependancy and built and styled the header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook MERN</h1>
+      <Header/>
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+/*Material UI Icons */
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import StoreOutlinedIcon from '@material-ui/icons/StoreOutlined';
+
+import { Avatar, IconButton} from '@material-ui/core';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import ForumOutlinedIcon from '@material-ui/icons/ForumOutlined';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import './header.css';
+
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header_left'>
+                <img src='https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.jpg'
+                alt='facebook-logo'/>
+            </div>
+
+            <div className='header_input'>
+                <SearchIcon/>
+                <input placeholder='Search Facebook' type='text'/>
+            </div>
+
+            <div className='header_center'>
+                <div className='header_option header_option--active'>
+                    <HomeIcon fontSize='large'/>
+                </div>
+                <div className='header_option'>
+                    <FlagIcon fontSize='large'/>
+                </div>
+                <div className='header_option'>
+                    <SubscriptionsOutlinedIcon fontSize='large'/>
+                </div>
+                <div className='header_option'>
+                    <SupervisedUserCircleIcon fontSize='large'/>
+                </div>
+                <div className='header_option'>
+                    <StoreOutlinedIcon fontSize='large'/>
+                </div>
+            </div>
+
+            <div className='header_right'>
+                <div className='header_info'>
+                    <Avatar/>
+                    <h4>Ridwan</h4>
+
+                    <IconButton>
+                        <AddOutlinedIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <ForumOutlinedIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <NotificationsIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <ExpandMoreIcon/>
+                    </IconButton>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
